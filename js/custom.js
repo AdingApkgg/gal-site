4fece1e2fe0421ccc614159aeff68dc1a56d83fa
AdingApkgg <AdingApkgg@outlook.com>
2024-01-30 11:36:38 +0800

    Site updated: 2024-01-30 11:36:37

--- js/custom.js
+++ js/custom.js
@@ -25,9 +25,9 @@
       snum = "0" + snum;
     }
     document.getElementById("timeDate").innerHTML =
-      "本站安全运行&nbsp" + dnum + "&nbsp天";
+      "本站居然运行了&nbsp" + dnum + "&nbsp天";
     document.getElementById("times").innerHTML =
-      hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
+      hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒！";
   }
 
   update();
