dd71a10fd5f4ea0d8e8713867f07083804e528e1
AdingApkgg <AdingApkgg@outlook.com>
2024-07-31 04:00:36 +0800

    Site updated: 2024-07-31 04:00:33

--- js/third-party/addtoany.js
+++ js/third-party/addtoany.js
@@ -1,1 +1,8 @@
-document.addEventListener("page:loaded",(()=>{NexT.utils.getScript("https://static.addtoany.com/menu/page.js",{condition:window.a2a}).then((()=>{window.a2a.init()}))}));
+/* global NexT */
+
+document.addEventListener('page:loaded', () => {
+  NexT.utils.getScript('https://static.addtoany.com/menu/page.js', { condition: window.a2a })
+    .then(() => {
+      window.a2a.init();
+    });
+});
