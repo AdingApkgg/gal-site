bf59bc282ce026c50dc62189fdc0187fe3c40d5d
AdingApkgg <AdingApkgg@outlook.com>
2025-10-06 01:06:09 +0800

    Site updated: 2025-10-06 01:06:04

--- js/third-party/addtoany.js
+++ js/third-party/addtoany.js
@@ -1,1 +1,1 @@
-document.addEventListener("page:loaded",()=>{NexT.utils.getScript("https://static.addtoany.com/menu/page.js",{condition:window.a2a}).then(()=>{window.a2a.init()})});
+document.addEventListener("page:loaded",async()=>{await NexT.utils.getScript("https://static.addtoany.com/menu/page.js",{condition:window.a2a}),window.a2a.init()});
